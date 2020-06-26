06097dfec4172b1a3ed0762bae958d661ff209b5
Ashley Richardson <ashley-richardson@lambdastudents.com>
2020-06-25 21:05:25 -0400

    new final

--- index.js
+++ index.js
@@ -93,10 +93,10 @@ Your function should accept:
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
 function getFlavorByIndex(array, index) {
-    var index = originalFlavors.indexOf("Black Walnut");
-    console.log(index);
+    array[index];
+    console.log(array[index]);
 }
-getFlavorByIndex(originalFlavors, "Black Walnut");
+getFlavorByIndex(originalFlavors, 2);
 
 
 /* Task 5: As corporate wants to add more and more flavors to their lineup, they've realized that they need to remove flavors based on flavor name, as opposed to just arbitrarily removing the first or last flavor. Your task is to get an index by flavor name, and remove that flavor from the array. 
@@ -157,7 +157,7 @@ function filterByWord(array, flavor) {
     }
 return newArray
 }
-console.log(filterByWord(originalFlavors, "Chocolate"));
+console.log(filterByWord(originalFlavors, "Lemon"));
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
 
